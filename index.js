842096f5ab4146f19bba515a62ecf07c9e55e937
retro <retropersik@ykr.net>
2023-04-24 14:28:02 +0300

    додано елемент списку, щоб виводити список людей прибрано пімпочку на списку, щоб не заважала копіювати додана строка щоб тестити додана функція яка розділяє строку на список людей

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const inputBtn = document.getElementById("input-btn")
-const splitEl = document.getElementById("split-el")
+const splitBtn = document.getElementById("split-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+
+
+let str = "впвапіп, вапвпіап, sdgdfgdfg"
+
+function split() {
+    const list = str.split(',')
+    for (let i = 0; i < list.length; i++) {
+        console.log(list[i])
+    }
+}
