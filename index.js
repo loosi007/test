d1afdbe3c24ed0b1bd2cfcc187fe3fa501cb026f
retro <retropersik@ykr.net>
2023-04-24 14:56:17 +0300

    прибрана функція split() строка тепер зчитується з поля вводу клік на кнопку ділить строку і виводить дані у вигляді списку

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const splitBtn = document.getElementById("split-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+let list = []
 
-
-let str = "впвапіп, вапвпіап, sdgdfgdfg"
-
-function split() {
-    const list = str.split(',')
-    for (let i = 0; i < list.length; i++) {
-        console.log(list[i])
+splitBtn.addEventListener("click", function (){
+    let str = inputEl.value
+    list = str.split(',')
+    for (let i = 0; i < list.length; i++){
+        const li = document.createElement("li")
+        li.textContent = list[i]
+        ulEl.append(li)
     }
-}
+})
